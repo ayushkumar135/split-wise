4d0e3778e96c26d13861329e1b5542c5ac49b927
i-ayush-singh <singh.ayush.6262@gmail.com>
2025-07-16 00:06:06 +0530

    server files added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,162 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { useEffect, useState } from "react";
+import Login from "./Components/Login";
+import axios from "axios";
+import NavBar from "./Components/NavBar";
+import SignUp from "./Components/SignUp";
+import LandingPage from "./Pages/LandingPage";
+import Home from "./Pages/Home";
+import Dashboard from "./Pages/DashBoard";
+import Groups from "./Pages/Groups";
+import AddGroup from "./Pages/Groups/Addgroup";
+import GroupDetail from "./Pages/Groups/GroupDetail";
+import Individual from "./Pages/Individual/Individual";
+import Friends from "./Pages/Friends/Friends";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import ExpenseReport from "./Pages/Individual/ExpenseReport";
+import ExpenseGroupReport from "./Pages/Groups/ExpenseReport";
+import FriendExpenseGroupReport from "./Pages/Friends/FriendExpenseReport";
+import { ContactUs } from "./Components/EmailInput";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { ThemeProvider } from "./context/theme";
+import ThemeBtn from "./Components/ThemeBtn";
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 3600000,
+    },
+  },
+});
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  // actual change in theme
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <GoogleOAuthProvider clientId="979404826140-4kr357pq71slm1j1as2f2j9p4ifmi40r.apps.googleusercontent.com">
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+          <BrowserRouter>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <div>
+                    <LandingPage />
+                  </div>
+                }
+              />
+              <Route
+                path="/home"
+                element={
+                  <div className="">
+                    <Dashboard />
+                    <Home />
+                  </div>
+                }
+              />
+              <Route
+                path="/groups"
+                element={
+                  <div className="">
+                    <Dashboard />
+                    <Groups />
+                  </div>
+                }
+              />
+              <Route
+                path="/group/detail/:groupId"
+                element={
+                  <div className="">
+                    <Dashboard />
+                    <GroupDetail />
+                  </div>
+                }
+              />
+              <Route
+                path="/addgroup"
+                element={
+                  <div className="">
+                    <Dashboard />
+                    <AddGroup />
+                  </div>
+                }
+              />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route
+                path="/individual"
+                element={
+                  <>
+                    <Dashboard />
+                    <Individual />
+                  </>
+                }
+              />
+              <Route
+                path="/expense/report/:id"
+                element={
+                  <>
+                    <Dashboard />
+                    <ExpenseReport />
+                  </>
+                }
+              />
+              <Route
+                path="/groupexpense/report/:id"
+                element={
+                  <>
+                    <Dashboard />
+                    <ExpenseGroupReport />
+                  </>
+                }
+              />
+              <Route
+                path="/friendexpense/report/:id"
+                element={
+                  <>
+                    <Dashboard />
+                    <FriendExpenseGroupReport />
+                  </>
+                }
+              />
+              <Route
+                path="/email/:email/:name"
+                element={
+                  <>
+                    <ContactUs />
+                  </>
+                }
+              />
+              <Route
+                path="/friends"
+                element={
+                  <div className="">
+                    <Dashboard />
+                    <Friends />
+                  </div>
+                }
+              />
+            </Routes>
+          </BrowserRouter>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </GoogleOAuthProvider>
+  );
 }
-
-export default App
+export default App;
